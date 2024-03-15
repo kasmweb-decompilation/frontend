fe5af881ce26f49e962a2e75b8d160d6948417ac
uhidontkno <hi@rare1k.dev>
2024-03-15 13:24:38 -0400

    feat: stuff

--- kasmweb/build.ts
+++ kasmweb/build.ts
@@ -3,14 +3,15 @@ const { build } = require('esbuild');
 const {sassPlugin} = require('esbuild-sass-plugin')
 const fs = require('fs-extra')
 const util = require('util');
-async function asyncESBuild() {
- await build({
+console.log("[ℹ️] Building...")
+ build({
     entryPoints: ['src/index.js'],
     bundle: true,
     logLimit: 0,
     outdir: 'dist',
-    logLevel:"error",
+    logLevel:"info",
     minify: true,
+    assetNames: 'assets/[name]-[hash]',
     plugins: [sassPlugin()],
     loader: {
       '.js': 'jsx',
@@ -26,25 +27,21 @@ async function asyncESBuild() {
       '.eot': 'file'
   
     }
-  })
-}
-console.log("[ℹ️] Building...")
-util.promisify(asyncESBuild
-);
-
-
-
-console.log("[✅] Build complete! Moving files...")
-
-  try {
-    (async()=>{
-    await fs.copy('public', 'dist');
-    console.log('[✅] Build complete!');
-    process.exit(0)
-  })
-
+  }).then(async dummy => {
+    try {
+      console.log("[ℹ️] Moving files from public to dist...")
+      await fs.copy('public', 'dist');
+      console.log('[✅] Build complete!');
+      process.exit(0)
     
-  } catch (err) {
-    console.error('[❌] Error copying public directory:', err);
-    process.exit(1);
-  }
+  
+      
+    } catch (err) {
+      console.error('[❌] Error copying public directory:', err);
+      process.exit(1);
+    }
+  
+  })
+
+
+
